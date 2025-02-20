89fd75f246ddd44756bc5b8929d6f8eec9f20470
AmauriGC <20233tn102@utez.edu.mx>
2025-02-19 22:42:39 -0600

    Inicio del APIrest noSQL

---
+++ src/test/java/com/sisgebi/SisgebiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisgebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgebiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
